532e7daaf524ade765fd9e364c3d99820d7236f8
37336 <pavana.n@tataelxsi.co.in>
2024-07-01 16:05:15 +0530

    API response file created

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -3,6 +3,7 @@ class AppException implements Exception {
   final _prefix;
   AppException([this._message, this._prefix]);
 
+  @override
   String toString() {
     return "$_prefix$_message";
   }
@@ -10,14 +11,19 @@ class AppException implements Exception {
 
 class FetchDataException extends AppException {
   FetchDataException([String? message])
-      : super(message, "Error During Communication: ");
+      : super(message, "Error During Communication");
 }
 
 class BadRequestException extends AppException {
-  BadRequestException([String? message]) : super(message, "Invalid Request: ");
+  BadRequestException([String? message]) : super(message, "Invalid Request");
 }
 
 class UnauthorizedException extends AppException {
   UnauthorizedException([String? message])
-      : super(message, "Unauthorized Request: ");
+      : super(message, "Unauthorized Request");
+}
+
+class InvalidinputException extends AppException {
+  InvalidinputException([String? message])
+      : super(message, "Invalid Input Request");
 }
